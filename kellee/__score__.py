39a82758a827577907045686c225963f1f4c948a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 06:01:55 -0300

    Created kellee/__score__.py at Timestamp: 2018-07-22 09:01:55

---
+++ kellee/__score__.py
@@ -0,0 +1,2 @@
+{
+"c005292d": {"author_nick": "nislei", "author_name": "nislei cassio martins", "author_email": "nislei@me.com", "author_org": "cederj", "author_site": "cederj.com.br", "author_public": "on"},[
