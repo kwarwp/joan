e50645b4a265796bfa7fbcf3621901307dd5a9df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 17:04:45 -0300

    Saved callie/main.py @2019-10-06 20:04

--- callie/main.py
+++ callie/main.py
@@ -1,11 +1,1 @@
-from _spy.vitollino.main import Cena
-
-inicio = "https://i.imgur.com/6rsRcPT.png"
-
-def cena1():
-
-    abacaxi= Cena(img=inicio)
-  
-    abacaxi.vai()
-
-cena1()
+# joan.callie.main.py
