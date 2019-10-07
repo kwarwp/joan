fc3973fbf3424895439f2b7af1c5a97bf19c08f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 11:06:42 -0300

    Created sara/main.py @2019-10-07 14:06

---
+++ sara/main.py
@@ -0,0 +1,6 @@
+# joan.sara.main.py
+from _spy.vitollino.main import cena
+
+cena_teste_raquel = "https://i.imgur.com/a9flzZX.jpg"
+cena_teste_raquel = cena (cena_teste_raquel)
+cena_teste_raquel.vai()
