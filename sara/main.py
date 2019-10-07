6b2ad0f3de2e7536b06c2fd812aed808f6e10bf6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 11:08:07 -0300

    Saved sara/main.py @2019-10-07 14:08

--- sara/main.py
+++ sara/main.py
@@ -1,6 +1,5 @@
 # joan.sara.main.py
 from _spy.vitollino.main import cena
-
-cena_teste_raquel = "https://i.imgur.com/a9flzZX.jpg"
-cena_teste_raquel = cena (cena_teste_raquel)
-cena_teste_raquel.vai()
+praia = "https://i.imgur.com/a9flzZX.jpg"
+praia = Cena (praia)
+praia.vai()
