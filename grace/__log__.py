28c2ae684b205b90007f31bd32b7696d0789f42f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-24 13:11:06 -0300

    Saved grace/__log__.py @2020-04-24 16:11

--- grace/__log__.py
+++ grace/__log__.py
@@ -44,3 +44,23 @@ NameError: name 'Papel' is not defined
     output("você escolheu Rocha")
 NameError: name 'output' is not defined
 '''},
+{'date': 'Fri Apr 24 2020 13:11:04.430 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    main()
+  module <module> line 60
+    escolheu = escolhe[escolha](seu_nome)
+  module <module> line 46
+    super()(dono)
+AttributeError: object 'super' has no attribute '__call__'
+'''},
