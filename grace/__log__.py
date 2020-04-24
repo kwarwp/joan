22f30ff8f395f903e3c915fab5b96ccb2d8047df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-24 12:34:37 -0300

    Created grace/__log__.py @2020-04-24 15:34

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Apr 24 2020 12:34:35.663 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 45
+  escolha = input("Digite r,p ou t para rocha, papel ou tesoura)
+                                                                ^
+SyntaxError: EOL while scanning string literal
+'''},
