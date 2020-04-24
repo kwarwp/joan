f64b2fe278e6977375b8caadc8b3991f16d5bba3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-24 12:34:54 -0300

    Saved grace/__log__.py @2020-04-24 15:34

--- grace/__log__.py
+++ grace/__log__.py
@@ -6,3 +6,21 @@
                                                                 ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Fri Apr 24 2020 12:34:53.639 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    main()
+  module <module> line 44
+    escolhe = dict(r=Rocha, p=Papel, t=Tesoura)
+NameError: name 'Papel' is not defined
+'''},
