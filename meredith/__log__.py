98a16b89344c34c1e407470383986b55087d367e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-04 13:27:25 -0300

    Created meredith/__log__.py @2020-05-04 16:27

---
+++ meredith/__log__.py
@@ -0,0 +1,5 @@
+
+{'date': 'Mon May 04 2020 13:27:23.684 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: innermost.C.tree[0].args.indexOf is not a function>
+'''},
