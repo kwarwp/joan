6f72b419387dc8df2b53520bc74b784a7121abf8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-07 12:06:41 -0300

    Created samantha/__log__.py @2020-05-07 15:06

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 07 2020 12:06:39.140 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from_supy.vitolino.main.import Cena,Elemento,Texto, STYLE
+                           ^
+SyntaxError: invalid syntax
+'''},
