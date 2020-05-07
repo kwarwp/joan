8c04f84b63d841e16c732cca1770ab1600d1fa7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-07 12:15:41 -0300

    Created samantha/main.py @2020-05-07 15:15

---
+++ samantha/main.py
@@ -0,0 +1,75 @@
+# joan.samantha.main.py
+# lorinda.kathryn.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE
+from _spy.vitollino.main import Inventario as inv
+
+STYLE["width"] = 800
+STYLE["height"] = "600px"
+
+#CENA 1 VERBO LEMBRAR HEREDITARIEDADE
+
+SALA_FIOCRUZ = "http://supygirls.pythonanywhere.com"
+ROSALINDA = "https://i.imgur.com/qvuwHvs.png"
+ZEZINHO = "https://i.imgur.com/94lhgKo.png"
+NPC = " https://i.imgur.com/NEG2o8W.jpg"
+PERGAMINHO = "https://i.imgur.com/Xe3zdhP.jpg"
+PESSOA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/6vgubFm.jpg"
+PESSOA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/wtpBiVS.jpg"
+PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/ZcfLShe.jpg"
+CRIANCA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/ISvWiD4.jpg"
+CRIANCA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/qoGRKc8.jpg"
+CRIANCA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/DtSbcLh.jpg"
+QUADRO_1_PESSOA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/6vgubFm.jpg"
+QUADRO_2_PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/6vgubFm.jpg"
+QUADRO_3_PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/ZcfLShe.jpg"
+QUADRO_4_CRIANCA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/ISvWiD4.jpg"
+QUADRO_5_CRIANCA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/qoGRKc8.jpg"
+QUADRO_6_CRIANCA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/DtSbcLh.jpg"
+MUSEU_DA_VIDA = "https://i.imgur.com/s4ZBrRv.jpg"
+DNA = "https://www.facebook.com/Projeto-SupyGirls-2062797764002029"
+Genes = "https://i.imgur.com/SBcbyUc.gif"
+Nucleotideos = "https://i.imgur.com/eUn2STG.png"
+'''
+Organismo
+Sequência correta
+Cofre
+Abrir cofre
+
+PenDrive
+
+Poção mágica
+
+Super poderes
+'''
+
+
+# O CÓDIGO DO MINE GAME ENTRA AQUI?
+class Primeira_fase():
+    def __init__(self):
+
+        sala_fiocruz = Cena(img= SALA_FIOCRUZ)
+
+'''
+O NPC DEVERÁ APRESENTAR AS SEGUINTES DICAS ORGANIZAR AS PALAVRAS UTILIZANDO A ORDEM CORRETA DE ENTENDIMENTO
+
+
+
+
+Nucleotídeos
+
+Características
+
+Organismo
+
+Sequência correta
+
+Abrir cofre
+
+PenDrive
+
+Poção mágica
+
+Super poderes
+'''
+if __name__ == "__main__":
+    Primeira_fase()
