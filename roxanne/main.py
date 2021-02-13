ae0d3f6455247a3b5eceb0712c5402f8220fe42d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 19:34:15 -0300

    Saved roxanne/main.py @2021-02-13 22:34

--- roxanne/main.py
+++ roxanne/main.py
@@ -8,12 +8,12 @@ linkWaka1 = "https://i.imgur.com/e641yTi.png"
 def jogo():
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
     okami1 = Elemento(img = linkOkami1, tit = "Okami 1",
-                         style = dict (y = 400, x = 60, h = 80, w = 160))
+                         style = dict (y = 400, x = 60, h = 200, w = 200))
     okami1.entra(cenaIlha)
     
     
     waka1 = Elemento(img = linkWaka1, tit = "Waka 1",
-                         style = dict (y = 300, x = 260, h = 100, w = 150))
+                         style = dict (y = 380, x = 340, h = 200, w = 200))
     waka1.entra(cenaIlha)
     txtwaka1 = Texto(cenaIlha, "vamos para a ilha das deusas")
     waka1.vai = txtwaka1.vai
