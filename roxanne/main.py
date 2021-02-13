49afe4091d946c85abc49c460d658dec9fd2ccda
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 20:15:46 -0300

    Saved roxanne/main.py @2021-02-13 23:15

--- roxanne/main.py
+++ roxanne/main.py
@@ -6,9 +6,12 @@ linkilhadeusas = "https://i.imgur.com/5UvVC5M.png"
 linkOkami1 = "https://i.imgur.com/hr6DsqU.png"
 linkWaka1 = "https://i.imgur.com/e641yTi.png"
 linkArk1 = "https://i.imgur.com/DhV0DOD.png"
+linkGruta1 = "https://4.bp.blogspot.com/-MplqVVRjFCo/VzYD90Cm5jI/AAAAAAAAX_Q/x2JbhgAwJwAi0cNTQq2l2j6hLCEX--gZgCLcB/s1600/Gruta%2Bde%2BBacaetava.jpg"
 
 def jogo():
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
+    cena_gruta = Cena(img = linkGruta1)
+    cenaIlha.meio = cena_gruta
     okami1 = Elemento(img = linkOkami1, tit = "Okami",
                          y = 400, x = 170, h = 200, w = 200)
     okami1.entra(cenaIlha)
