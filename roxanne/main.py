9c16419122bfe90f96b00e6712771967bf599dd2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 18:46:04 -0300

    Saved roxanne/main.py @2021-02-13 21:46

--- roxanne/main.py
+++ roxanne/main.py
@@ -6,8 +6,10 @@ linkOkami1 = "https://i.imgur.com/hr6DsqU.png"
 
 def jogo():
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
-    Okami1 = Elemento(img = linkOkami1, tit = "Okami 1",
+    okami1 = Elemento(img = linkOkami1, tit = "Okami 1",
                          style = dict (top = 200, left = 60, height = 100, width = 60))
-    Okami1.entra(cenaIlha)
+    okami1.entra(cenaIlha)
+    texto1 = Texto(cenaIlha, "vamos para a ilha das deusas")
+    okami1.vai = txtokami1.vai
     cenaIlha.vai()
 jogo()
