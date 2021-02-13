8c45bcf432515533c9928ff1c918a7b024d6610d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 18:46:07 -0300

    Saved roxanne/__log__.py @2021-02-13 21:46

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -17,3 +17,21 @@
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sat Feb 13 2021 18:46:10.95 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    jogo()
+  module <module> line 13
+    okami1.vai = txtokami1.vai
+NameError: name 'txtokami1' is not defined
+'''},
