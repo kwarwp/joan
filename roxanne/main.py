ba6cfec9fda156d3d3cc8059f273c503b4db4e4d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 19:53:44 -0300

    Saved roxanne/main.py @2021-02-13 22:53

--- roxanne/main.py
+++ roxanne/main.py
@@ -5,18 +5,24 @@ STYLE.update(width=1150, height="600px")
 linkilhadeusas = "https://i.imgur.com/5UvVC5M.png"
 linkOkami1 = "https://i.imgur.com/hr6DsqU.png"
 linkWaka1 = "https://i.imgur.com/e641yTi.png"
+linkArk1 = "https://i.imgur.com/DhV0DOD.png"
+
 def jogo():
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
     okami1 = Elemento(img = linkOkami1, tit = "Okami 1",
                          y = 400, x = 60, h = 200, w = 200)
     okami1.entra(cenaIlha)
-    
+     
     
     waka1 = Elemento(img = linkWaka1, tit = "Waka 1",
                          y = 340, x = 340, h = 250, w = 200)
     waka1.entra(cenaIlha)
     txtwaka1 = Texto(cenaIlha, "Vamos para a ilha das deusas.")
     waka1.vai = txtwaka1.vai
+    
+    ark1 = Elemento(img = linkArk1, tit = "Ark 1",
+                         y = 400, x = 60, h = 200, w = 200)
+    ark1.entra(cenaIlha)
     
     cenaIlha.vai()
 jogo()
