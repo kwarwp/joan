858df6bf5ef62b632fadfa3d4b39036933904b9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 19:55:26 -0300

    Saved roxanne/main.py @2021-02-13 22:55

--- roxanne/main.py
+++ roxanne/main.py
@@ -10,7 +10,7 @@ linkArk1 = "https://i.imgur.com/DhV0DOD.png"
 def jogo():
     cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
     okami1 = Elemento(img = linkOkami1, tit = "Okami 1",
-                         y = 400, x = 60, h = 200, w = 200)
+                         y = 400, x = 100, h = 200, w = 200)
     okami1.entra(cenaIlha)
      
     
