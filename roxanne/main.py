13141c64178eb15d177e9a257f215adfe0fdc797
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 18:04:34 -0300

    Created roxanne/main.py @2021-02-13 21:04

---
+++ roxanne/main.py
@@ -0,0 +1,9 @@
+# Miguel Roman-Okami Shiranui
+from _spy.vittolino.main import Cena, Elemento, Texto
+
+linkilhadeusas = "https://i.imgur.com/5UvVC5M.png"
+linkokami1 = "https://i.imgur.com/hr6DsqU.png"
+
+def jogo():
+    cenaIlha = Cena(img = "https://i.imgur.com/5UvVC5M.png")
+    cenaIlha.vai()
