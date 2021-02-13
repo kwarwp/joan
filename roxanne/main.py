afee763ba11e56fc79800ec033fdbe46bee5a263
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 18:17:29 -0300

    Saved roxanne/main.py @2021-02-13 21:17

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,5 +1,5 @@
 # Miguel Roman-Okami Shiranui
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 
 linkilhadeusas = "https://i.imgur.com/5UvVC5M.png"
 linkokami1 = "https://i.imgur.com/hr6DsqU.png"
