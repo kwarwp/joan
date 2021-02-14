4d7eb6b1afe3f932a563945e217418931e1b8f13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 21:15:06 -0300

    Saved roxanne/main.py @2021-02-14 00:15

--- roxanne/main.py
+++ roxanne/main.py
@@ -28,7 +28,7 @@ def jogo():
                          y = 400, x = 40, h = 200, w = 500)
     ark1.entra(cenaIlha)
     
-    rat1 = Elemento(img = linkRat1, tit = "Rat god",
+    rat1 = Elemento(img = linkRat1, tit = "Rat godess",
                          y = 300, x = 170, h = 400, w = 400)
     rat1.entra(cena_gruta)
     ark2 = Elemento(img = linkArk2, tit = "Ark",
