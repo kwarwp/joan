a92ececc57ba5e58e08d6f0e1ff5b153e4728d47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-14 17:40:56 -0300

    Saved roxanne/__log__.py @2021-02-14 20:40

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -313,3 +313,21 @@ UnboundLocalError: local variable 'espada1' referenced before assignment
     espada1.entra(Cena_gruta) 
 UnboundLocalError: local variable 'espada1' referenced before assignment
 '''},
+{'date': 'Sun Feb 14 2021 17:40:56.430 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    jogo()
+  module <module> line 71
+    espada1.vai = cena_Blockhead.vai
+NameError: name 'cena_Blockhead' is not defined
+'''},
