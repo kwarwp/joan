04bfe50491fa8f02b1e7191a46f66f1412a1640e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 22:47:33 -0300

    Saved courtney/__log__.py @2021-02-14 01:47

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -17,3 +17,21 @@
     vai_guta.vai = gruta
 NameError: name 'vai_guta' is not defined
 '''},
+{'date': 'Sat Feb 13 2021 22:47:32.520 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    jogo()
+  module <module> line 52
+    invent.elt.style.update(width="60px", height="60px")
+AttributeError: 'CSSProperty' object has no attribute 'update'
+'''},
