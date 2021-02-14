5fa24f68593d5ab9f9e7d668416dce9dc36b1a8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-14 15:55:34 -0300

    Saved roxanne/__log__.py @2021-02-14 18:55

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -92,3 +92,19 @@ IndentationError: unexpected indent
     mapa = Elemento(img = linkMap, tit = "Mapa", h = 200, w = 200)
 NameError: name 'linkMap' is not defined
 '''},
+{'date': 'Sun Feb 14 2021 15:55:34.569 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    ESTILE.update(width="60px", height="60px", minHeight="60px")
+NameError: name 'ESTILE' is not defined
+'''},
