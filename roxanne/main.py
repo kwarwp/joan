6292ca6a73a91c9b8a3744af43dda7adbcdd151f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-13 21:10:18 -0300

    Saved roxanne/main.py @2021-02-14 00:10

--- roxanne/main.py
+++ roxanne/main.py
@@ -33,7 +33,7 @@ def jogo():
                          y = 300, x = 170, h = 400, w = 400)
     rat1.entra(cena_gruta)
     ark2 = Elemento(img = linkArk2, tit = "Ark",
-                         y = 350, x = 810, h = 100, w = 100)
+                         y = 350, x = 830, h = 100, w = 100)
     ark2.entra(cena_gruta)  
     cena_gruta.vai()
 jogo()
