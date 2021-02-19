107b3ed0ea56671ae9caf8f15bad609582f5bcb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-19 07:37:06 -0300

    Saved roxanne/__log__.py @2021-02-19 10:37

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -385,3 +385,21 @@ NameError: name 'linkCenalago' is not defined
     cena_lago = Cena(img = linkCenalago)
 NameError: name 'linkCenalago' is not defined
 '''},
+{'date': 'Fri Feb 19 2021 07:37:05.629 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    jogo()
+  module <module> line 37
+    cena_lag
+NameError: name 'cena_lag' is not defined
+'''},
