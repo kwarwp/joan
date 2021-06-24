11db406ef9515545649bf7b19c66eac09a7843db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-24 11:31:11 -0300

    Saved grace/__log__.py @2021-06-24 14:31

--- grace/__log__.py
+++ grace/__log__.py
@@ -116,3 +116,23 @@ SyntaxError: invalid syntax
     self.cibele = J.a(self.CIBELE, y=400, w=200, h=250, cena=self.isaias)
 AttributeError: 'Isaias' object has no attribute 'CIBELE'
 '''},
+{'date': 'Thu Jun 24 2021 11:31:10.594 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    Joao().vai()
+  module <module> line 25
+    self.isaias = Isaias()
+  module <module> line 51
+    self.isaias.esquerda = J.c("", vai=self.voz)
+AttributeError: 'Isaias' object has no attribute 'voz'
+'''},
