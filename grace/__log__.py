c9d447e446fe0d6627b583f0cfc941cffd548a77
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-24 10:52:27 -0300

    Saved grace/__log__.py @2021-06-24 13:52

--- grace/__log__.py
+++ grace/__log__.py
@@ -96,3 +96,23 @@ TypeError: __init__() got multiple values for argument 'cena'
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jun 24 2021 10:52:27.116 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    Joao().vai()
+  module <module> line 25
+    self.isaias = Isaias()
+  module <module> line 44
+    self.cibele = J.a(self.CIBELE, y=400, w=200, h=250, cena=self.isaias)
+AttributeError: 'Isaias' object has no attribute 'CIBELE'
+'''},
