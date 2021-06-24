9df30dd475dba24a186f1f364ce9fd6f7e567e6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-24 12:32:21 -0300

    Saved grace/__log__.py @2021-06-24 15:32

--- grace/__log__.py
+++ grace/__log__.py
@@ -186,3 +186,21 @@ AttributeError: 'Zacarias' object has no attribute 'deserto'
                                                                                                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jun 24 2021 12:32:20.650 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 126
+    Zacarias().vai()
+  module <module> line 104
+    self.anuncia.esquerda = J.c("", vai=self.voz)
+AttributeError: 'Zacarias' object has no attribute 'voz'
+'''},
