fd705bf6d51ae2dd8f71bd39d7f49af6a3572796
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 11:27:02 -0300

    Saved angie/main.py @2021-07-05 14:27

--- angie/main.py
+++ angie/main.py
@@ -1,1 +1,3 @@
 # Cibele Ribeiro
+from _spy.vittolino.main import Cena, Elemento, Texto
+linkFloresta = "https://a-static.mlcdn.com.br/618x463/painel-de-festa-em-tecido-sublimado-3d-caminho-na-floresta-sublime-sonhos/sublimesonhos/614357441/6a02b48244ffbcef0bf683dd4997e275.jpg"
