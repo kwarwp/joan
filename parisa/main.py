e6190f46a2cac1d55a50d31d29b000c328159599
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 11:27:01 -0300

    Saved parisa/main.py @2021-07-05 14:27

--- parisa/main.py
+++ parisa/main.py
@@ -1,1 +1,3 @@
 # Nara de Faria e Sousa Cardenuto
+from _spy.vittolino.main import Cena, Elemento, Texto
+linkFlorestasemnada = "https://image.freepik.com/vetores-gratis/paisagem-de-floresta-dos-desenhos-animados-com-plantas-e-arvores_43633-5886.jpg
