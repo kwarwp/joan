f15d8f0d0b3834e51edc8588daa5028af31c7d88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-13 10:58:51 -0300

    Saved angie/main.py @2021-07-13 13:58

--- angie/main.py
+++ angie/main.py
@@ -1,5 +1,5 @@
 # Cibele Ribeiro
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 linkFloresta = "https://a-static.mlcdn.com.br/618x463/painel-de-festa-em-tecido-sublimado-3d-caminho-na-floresta-sublime-sonhos/sublimesonhos/614357441/6a02b48244ffbcef0bf683dd4997e275.jpg"
 linkFairy = "https://thefairyprincess.net/wp-content/uploads/2020/03/1_rev-min-724x1024.png"
 # I love my forest!
