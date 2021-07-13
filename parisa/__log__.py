214dc12f140b22ccff99354370033feaa929965d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-13 11:30:49 -0300

    Saved parisa/__log__.py @2021-07-13 14:30

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -69,3 +69,21 @@ NameError: name 'Elemto' is not defined
     Mulher.entra(cenaFloresta)
 NameError: name 'Mulher' is not defined
 '''},
+{'date': 'Tue Jul 13 2021 11:36:26.390 GMt-0300 (BRT) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    Historia()
+  module <module> line 11
+    mlher.entra(cenaFloresta)
+NameError: name 'mlher' is not defined
+'''},
