f0e781a3f4df13a06f904b5df6751b265c524c3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-16 10:53:16 -0300

    Saved parisa/main.py @2021-07-16 13:53

--- parisa/main.py
+++ parisa/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 linkFlorestasemnada = "https://image.freepik.com/vetores-gratis/paisagem-de-floresta-dos-desenhos-animados-com-plantas-e-arvores_43633-5886.jpg"
 linkMulher = "https://i.imgur.com/5M7D5s5.png"
-# Hi! Today i'm doing a trail in this forest.
+# Hi! Today I'm hiking in this forest.
 def Historia():
     cenaFloresta = Cena(img = linkFlorestasemnada)
     mulher = Elemento(img = linkMulher,
