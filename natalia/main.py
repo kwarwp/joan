5ad10470e9b82e32fad68d2ba9f61e6e64c945b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:46:03 -0300

    Created natalia/main.py @2021-08-09 18:46

---
+++ natalia/main.py
@@ -0,0 +1,4 @@
+# Leonardo Sassone 
+from _spy.vittolino.main import Cena, Elemento, Texto
+linkdonagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
+linkamegurake = "https://static.wikia.nocookie.net/naruto/images/4/41/Amegakure.png/revision/latest?cb=20120428143733&path-prefix=pt-br"
