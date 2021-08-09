c3cc1e274909c944c4a15adf5e1243f42e1818dc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:53:48 -0300

    Saved natalia/main.py @2021-08-09 18:53

--- natalia/main.py
+++ natalia/main.py
@@ -2,3 +2,5 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 linkdonagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
 linkamegurake = "https://static.wikia.nocookie.net/naruto/images/4/41/Amegakure.png/revision/latest?cb=20120428143733&path-prefix=pt-br"
+# I need to practice to defeat Hanzõ! 
+def Historia( ) :
