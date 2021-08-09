57466a93769db08b498257f4c8f754af50392be7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:59:35 -0300

    Saved natalia/__log__.py @2021-08-09 18:59

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                    ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Aug 09 2021 15:59:36.845 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  
+  ^
+IndentationError: expected an indented block
+'''},
