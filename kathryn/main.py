74e768374e9f73ce6e35323f89d1e88e2bc192b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:46:11 -0300

    Created kathryn/main.py @2021-08-09 18:46

---
+++ kathryn/main.py
@@ -0,0 +1,5 @@
+# Sofia Marques 
+from _spy.vittolino.main import Cena, Elemento, Texto
+
+linkdofundo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/07/03/16/van-gogh-sunflowers-3.jpg"
+linkdodetetive: "https://www.alunira.com/IMG/arton77.png?1472829267"
