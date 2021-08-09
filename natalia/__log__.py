b51dec6976b71a1793e280695bcc75caba233570
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:07:14 -0300

    Created natalia/__log__.py @2021-08-09 18:07

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 09 2021 15:07:13.483 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  After desapear shopy girls
+         ^
+SyntaxError: invalid syntax
+'''},
