1859efb1ad81ea739c24ce060d3cfc1fbe1c1e62
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 16:00:55 -0300

    Saved callie/main.py @2021-08-09 19:00

--- callie/main.py
+++ callie/main.py
@@ -1,4 +1,4 @@
 #Alice
-from _spy.vittolino.main import Cena, Elemento, Texto
-linkoftheocean:"https://www.google.com.br/search?q=drawing+girl+beach+boat&tbm=isch&ved=2ahUKEwj04ajNzaTyAhVZrZUCHYFFCf4Q2-cCegQIABAA&oq=drawing+girl+beach+boat&gs_lcp=CgNpbWcQAzoGCAAQCBAeUMutAliivAJgqL4CaABwAHgAgAGiAYgBvwWSAQMwLjWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=z3gRYbS-Ftna1sQPgYul8A8&bih=663&biw=1366#imgrc=yiaackQ6PJkp9M&imgdii=_SFaVUKSsRVV8M"
-def história 
+from _spy.vittolino.main import Cena, Elemento, 
+linkoftheboatandgirl: :"https://www.google.com.br/search?q=drawing+girl+beach+boat&tbm=isch&ved=2ahUKEwj04ajNzaTyAhVZrZUCHYFFCf4Q2-cCegQIABAA&oq=drawing+girl+beach+boat&gs_lcp=CgNpbWcQAzoGCAAQCBAeUMutAliivAJgqL4CaABwAHgAgAGiAYgBvwWSAQMwLjWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=z3gRYbS-Ftna1sQPgYul8A8&bih=663&biw=1366#imgrc=yiaackQ6PJkp9M&imgdii=_SFaVUKSsRVV8M"
+linkoftheocean:"https://www.google.com.br/search?q=mar+calmo&sxsrf=ALeKk03VaEzoA2tMaZBtN3WEthC5N4xRNA:1628534594152&source=lnms&tbm=isch&sa=X&ved=2ahUKEwil6_OPzKTyAhXJqpUCHf2pBioQ_AUoAXoECAEQAw&biw=1366&bih=663#imgrc=l18H6tdtfhs4wM" 
