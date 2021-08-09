90b3a9264eb5404099c68af59044487d44b0557a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:58:35 -0300

    Saved callie/main.py @2021-08-09 18:58

--- callie/main.py
+++ callie/main.py
@@ -1,1 +1,4 @@
-# joan.callie.main.py
+#Alice
+from _spy.vittolino.main import Cena, Elemento, Texto
+linkoftheocean:"https://www.google.com.br/search?q=drawing+girl+beach+boat&tbm=isch&ved=2ahUKEwj04ajNzaTyAhVZrZUCHYFFCf4Q2-cCegQIABAA&oq=drawing+girl+beach+boat&gs_lcp=CgNpbWcQAzoGCAAQCBAeUMutAliivAJgqL4CaABwAHgAgAGiAYgBvwWSAQMwLjWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=z3gRYbS-Ftna1sQPgYul8A8&bih=663&biw=1366#imgrc=yiaackQ6PJkp9M&imgdii=_SFaVUKSsRVV8M"
+def história 
