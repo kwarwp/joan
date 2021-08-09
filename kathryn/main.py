d3ca2f510fe7d150c3d63d68af92f37d3a046195
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 15:57:46 -0300

    Saved kathryn/main.py @2021-08-09 18:57

--- kathryn/main.py
+++ kathryn/main.py
@@ -3,3 +3,8 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 
 linkdofundo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/07/03/16/van-gogh-sunflowers-3.jpg"
 linkdodetetive: "https://www.alunira.com/IMG/arton77.png?1472829267"
+
+# Acaba de acontecer um roubo aqui no museu do Van Gogh!
+
+def Histori() :
+    
