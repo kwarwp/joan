1aedbbddfdb327cd00bb46a200911eab9d5590f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-09 08:40:02 -0300

    fix Okami lisa

--- __author__.py
+++ __author__.py
@@ -1,8 +1,8 @@
 {
-"courtney": {"author_nick": "courtney 2021", "author_name": "courtney 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
+"courtney": {"author_nick": "Okami 2021", "author_name": "Okami 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "angie": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "kathryn": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "natalia": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "parisa": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
 "callie": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
-"liza": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
+"lisa": {"author_nick": "StartAgora 2021", "author_name": "StartAgora 2021", "author_email": "", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
