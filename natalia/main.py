54c868338077f412eb571b205a3ce6ea30196b81
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:44:16 -0300

    Saved natalia/main.py @2021-08-11 18:44

--- natalia/main.py
+++ natalia/main.py
@@ -1,7 +1,7 @@
 # Leonardo Sassone 
 from _spy.vitollino.main import Cena, Elemento, Texto
 linkdoNagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
-linkKonohagurake = "https://static.wikia.nocookie.net/naruto/images/f/f2/Konohagakure_no_Sato.png/revision/latest?cb=20140809020103&path-prefix=pt-br"
+linkKonohagurake = "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Konohagakure.jpg"
 linkHanzo = "https://static.wikia.nocookie.net/naruto/images/d/d6/Hanz%C5%8D_%28Infobox_-_Geral%29.png/revision/latest/scale-to-width-down/250?cb=20140412234945&path-prefix=pt-br"
 # I need to practice to defeat Hanzõ! 
 def Historia(): 
