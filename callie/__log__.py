40356764b94b9e3814b49d7d61c8a36c79ed8a15
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:44:22 -0300

    Created callie/__log__.py @2021-08-11 18:44

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 11 2021 15:44:21.789 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  linkoftheboatandgirl: :"https://static.vecteezy.com/ti/vetor-gratis/p1/2141093-personagem-de-desenho-animado-rowing-the-boat-isolated-gr%C3%A1tis-vetor.jpg"
+                       ^
+SyntaxError: invalid syntax
+'''},
