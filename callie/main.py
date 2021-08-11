2ee59b3522121d458bb6a7fac574420b3631c8de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:49:30 -0300

    Saved callie/main.py @2021-08-11 18:49

--- callie/main.py
+++ callie/main.py
@@ -1,8 +1,9 @@
 #Alice
-from _spy.vittolino.main import Cena, Elemento, Texto
-linkoftheboatandgirl: :"https://static.vecteezy.com/ti/vetor-gratis/p1/2141093-personagem-de-desenho-animado-rowing-the-boat-isolated-gr%C3%A1tis-vetor.jpg"
-linkoftheoceanstorm:"https://ike.io/content/images/2018/08/the_sea-1.jpg" 
-def hitoria():
+from _spy.vitollino.main import Cena, Elemento, Texto
+linkoftheboatandgirl="https://static.vecteezy.com/ti/vetor-gratis/p1/2141093-personagem-de-desenho-animado-rowing-the-boat-isolated-gr%C3%A1tis-vetor.jpg"
+linkoftheoceanstorm="https://ike.io/content/images/2018/08/the_sea-1.jpg" 
+def historia():
+    stormCena=Cena(img=linkoftheoceanstorm)
 #Mary is salling for a long time. At the morning, she was just on a trip, but she went too far, and now, 4:00pm, Mary is fighting agains a big storm.
+    stormCena.vai
 historia()
-stormCena=Cena(img=linkoftheoceanstorm)
