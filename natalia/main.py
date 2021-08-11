da364fee22d3ca7831e33d283ddaa240d1ffdb1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:40:49 -0300

    Saved natalia/main.py @2021-08-11 18:40

--- natalia/main.py
+++ natalia/main.py
@@ -1,5 +1,5 @@
 # Leonardo Sassone 
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 linkdoNagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
 linkAmegurake = "https://static.wikia.nocookie.net/naruto/images/4/41/Amegakure.png/revision/latest?cb=20120428143733&path-prefix=pt-br"
 linkHanzo = "https://static.wikia.nocookie.net/naruto/images/d/d6/Hanz%C5%8D_%28Infobox_-_Geral%29.png/revision/latest/scale-to-width-down/250?cb=20140412234945&path-prefix=pt-br"
