1ba4a6afaacbe66fc53ea6dde71228ddec17fada
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:37:01 -0300

    Saved kathryn/__log__.py @2021-08-11 18:37

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -6,3 +6,10 @@
               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 11 2021 15:37:03.42 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  linkdofundo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/07/03/16/van-gogh-sunflowers-3.jpg"
+              ^
+SyntaxError: invalid syntax
+'''},
