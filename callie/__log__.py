87e02e17c6412f528b957f5389220c6cf58dd7f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:46:33 -0300

    Saved callie/__log__.py @2021-08-11 18:46

--- callie/__log__.py
+++ callie/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 11 2021 15:46:34.785 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  stormCena=Cena(img=linkoftheoceanstorm)
+  ^
+IndentationError: expected an indented block
+'''},
