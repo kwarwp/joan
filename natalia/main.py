f572026dc66412ca6839b0622774a88910033adb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:36:20 -0300

    Saved natalia/main.py @2021-08-11 18:36

--- natalia/main.py
+++ natalia/main.py
@@ -1,7 +1,9 @@
 # Leonardo Sassone 
 from _spy.vittolino.main import Cena, Elemento, Texto
-linkdonagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
-linkamegurake = "https://static.wikia.nocookie.net/naruto/images/4/41/Amegakure.png/revision/latest?cb=20120428143733&path-prefix=pt-br"
+linkdoNagato = "https://i.pinimg.com/originals/3e/01/0c/3e010c0edfe7e2a982c421b06911d56f.png"
+linkAmegurake = "https://static.wikia.nocookie.net/naruto/images/4/41/Amegakure.png/revision/latest?cb=20120428143733&path-prefix=pt-br"
+linkHanzo = "https://static.wikia.nocookie.net/naruto/images/d/d6/Hanz%C5%8D_%28Infobox_-_Geral%29.png/revision/latest/scale-to-width-down/250?cb=20140412234945&path-prefix=pt-br"
 # I need to practice to defeat Hanzõ! 
-def Historia( ) : 
-    
+def Historia(): 
+    cenaAmegurake = Cena(img = linkAmegurake) 
+Historia(): 
