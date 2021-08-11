0b3e7219faf4f95aa81788518ce781eefc4a4539
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-11 15:42:49 -0300

    Saved kathryn/main.py @2021-08-11 18:42

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,8 +1,8 @@
 # Sofia Marques 
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-linkdofundo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/07/03/16/van-gogh-sunflowers-3.jpg"
-linkdodetetive: "https://www.alunira.com/IMG/arton77.png?1472829267"
+linkdofundo:"https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/07/03/16/van-gogh-sunflowers-3.jpg"
+linkdodetetive:"https://www.alunira.com/IMG/arton77.png?1472829267"
 
 # Acaba de acontecer um roubo aqui no museu do Van Gogh!
 
