3facf3846755a232f6bd987297c6e3041429299b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:56:22 -0300

    Saved callie/main.py @2021-08-16 18:56

--- callie/main.py
+++ callie/main.py
@@ -1,6 +1,6 @@
 #Alice
 from _spy.vitollino.main import Cena, Elemento, Texto
-linkoftheboatandgirl="https://static.vecteezy.com/ti/vetor-gratis/p1/2141093-personagem-de-desenho-animado-rowing-the-boat-isolated-gr%C3%A1tis-vetor.jpg"
+linkoftheboatandgirl="https://i.imgur.com/bLZMF23.png"
 linkoftheoceanstorm="https://ike.io/content/images/2018/08/the_sea-1.jpg" 
 def Historia():
     stormCena=Cena(img=linkoftheoceanstorm)
