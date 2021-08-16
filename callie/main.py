6cce15446766d6cc03972fd9ceceadfb7a9d2788
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:56:46 -0300

    Saved callie/main.py @2021-08-16 18:56

--- callie/main.py
+++ callie/main.py
@@ -7,7 +7,7 @@ def Historia():
 #Mary is salling for a long time. At the morning, she was just on a trip, but she went too far, and now, 4:00pm, Mary is fighting agains a big storm.
     mary=Elemento(img=linkoftheboatandgirl,
                  tit="Mary",
-                 style=dict(left=90, top=60, width=200, height=100))
+                 style=dict(left=90, top=60, width=220, height=100))
     mary.entra(stormCena)
     stormCena.vai()
     
