c83ac1bc120743a8cd5cf8321fd21d2a43f70e0d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:29:02 -0300

    Saved kathryn/__log__.py @2021-08-16 18:29

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -91,3 +91,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Aug 16 2021 15:29:09.931 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  detetive = elemento (img = linkdodetetive
+                                                                     ^
+SyntaxError: invalid syntax
+'''},
