7faa06701ded23abcc099ff50a203b34de720c72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 17:15:24 -0300

    Saved natalia/__log__.py @2021-08-16 20:15

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -211,3 +211,10 @@ SyntaxError: invalid syntax
                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Aug 16 2021 17:15:23.543 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  " I need to practice so I can defeat Sasuke, Datebbayo!
+                                                         ^
+SyntaxError: EOL while scanning string literal
+'''},
