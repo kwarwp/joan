e9658d19a18cd5154e4996b68e7c75751b696801
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:37:34 -0300

    Saved natalia/__log__.py @2021-08-16 18:37

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -120,3 +120,10 @@ SyntaxError: invalid syntax
                                                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 16 2021 15:37:34.489 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  nagato = Elemento(img=linkdoNagato,
+                                                                                                                                ^
+SyntaxError: invalid syntax
+'''},
