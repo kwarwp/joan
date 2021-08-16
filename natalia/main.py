fa364267f75a7c1c389b519854d9d7688df9ef04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 17:22:18 -0300

    Saved natalia/main.py @2021-08-16 20:22

--- natalia/main.py
+++ natalia/main.py
@@ -1,17 +1,19 @@
 
 # Leonardo Sassone 
 from _spy.vitollino.main import Cena, Elemento, Texto
-linkDoNagato = "https://i.imgur.com/aHGM7mE.png"
+linkDoNaruto = "https://i.imgur.com/aHGM7mE.png"
 linkKonohagurake = "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Konohagakure.jpg"
 linkHanzo = "http://images2.wikia.nocookie.net/__cb20120802150510/naruto/images/8/85/Hanzo_wielding_Kusarigama.png"
 # I need to practice to defeat Hanzõ! 
 def Historia(): 
     cenaKonohagurake=Cena(img=linkKonohagurake) 
     
-    nagato = Elemento(img=linkDoNagato,
-                     tit="Nagato",
+    naruto = Elemento(img=linkDoNaruto,
+                     tit="naruto",
                      style=dict(left=75, top=60, width=150, height=200))
-    nagato.entra(cenaKonohagurake)
+    naruto.entra(cenaKonohagurake)
     cenaKonohagurake.vai()
     
 Historia() 
+
+# No canto da dobra da perna esquerda, se eu botar a seta do mouse ali vai aparecer Naruto pequeno, mas aparece
