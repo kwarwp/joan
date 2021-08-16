c0e85fa30576eb887d66b90d950347e25022f2b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:57:45 -0300

    Saved natalia/main.py @2021-08-16 18:57

--- natalia/main.py
+++ natalia/main.py
@@ -1,15 +1,16 @@
+
 # Leonardo Sassone 
 from _spy.vitollino.main import Cena, Elemento, Texto
-linkdoNagato = "https://e7.pngegg.com/pngimages/773/342/png-clipart-naruto-uzumaki-sasuke-uchiha-hashirama-senju-kurama-naruto.png"
+linkDoNagato = "https://i.imgur.com/aHGM7mE.png"
 linkKonohagurake = "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Konohagakure.jpg"
 linkHanzo = "http://images2.wikia.nocookie.net/__cb20120802150510/naruto/images/8/85/Hanzo_wielding_Kusarigama.png"
 # I need to practice to defeat Hanzõ! 
 def Historia(): 
     cenaKonohagurake=Cena(img=linkKonohagurake) 
     
-    nagato=Elemento(img=linkdoNagato,
-                    tit="Nagato",
-                    style=dict(left=150, top=60, width=, height=200))
+    nagato = Elemento(img=linkDoNagato,
+                     tit="Nagato",
+                     style=dict(left=75, top=60, width=150, height=200))
     nagato.entra(cenaKonohagurake)
     cenaKonohagurake.vai()
     
