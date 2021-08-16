5f060030981410bb16efe3f44d0d04a6d649a540
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-16 15:48:09 -0300

    Saved callie/main.py @2021-08-16 18:48

--- callie/main.py
+++ callie/main.py
@@ -5,7 +5,7 @@ linkoftheoceanstorm="https://ike.io/content/images/2018/08/the_sea-1.jpg"
 def Historia():
     stormCena=Cena(img=linkoftheoceanstorm)
 #Mary is salling for a long time. At the morning, she was just on a trip, but she went too far, and now, 4:00pm, Mary is fighting agains a big storm.
-    mary=Elemento(img=linkoftheboatandthegirl,
+    mary=Elemento(img=linkoftheboatandgirl,
                  tit="Mary",
                  style=dict(left=150, top=60, width=60, height=200))
     mary.entra(stormCena)
