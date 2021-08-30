9938448df5148ab727bfa984ed045b8f901247fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 15:50:49 -0300

    Saved callie/__log__.py @2021-08-30 18:50

--- callie/__log__.py
+++ callie/__log__.py
@@ -95,3 +95,21 @@ NameError: name 'linkoftheboatandthegirl' is not defined
                                                                                                                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Aug 30 2021 15:50:47.670 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    Historia()
+  module <module> line 12
+    txtstartstorm= Texto(stormcena,
+NameError: name 'stormcena' is not defined
+'''},
