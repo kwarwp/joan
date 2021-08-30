969c440e0e95d0be161cac5743385e679b3b7bc9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 15:37:04 -0300

    Saved kathryn/__log__.py @2021-08-30 18:37

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -147,3 +147,10 @@ SyntaxError: invalid syntax
                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 30 2021 15:37:03.918 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  detetive = Elemento(img=linkdodetetive,
+                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
