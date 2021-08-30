1e196f52515b9520ffb91a14a3d5a97827babadc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 15:44:51 -0300

    Saved kathryn/__log__.py @2021-08-30 18:44

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -168,3 +168,10 @@ SyntaxError: invalid syntax
                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 30 2021 15:44:50.987 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  txtDetective = Texto(cena1,
+                                                                                                                     ^
+SyntaxError: invalid syntax
+'''},
