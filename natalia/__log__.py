cf9fd902bf619ebe5e535f32517e3a4438e59b61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 15:44:56 -0300

    Saved natalia/__log__.py @2021-08-30 18:44

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -239,3 +239,10 @@ SyntaxError: invalid syntax
                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 30 2021 15:45:00.779 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  txtNaruto = Texto(cenaKonohagurake,
+                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
