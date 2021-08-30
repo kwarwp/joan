165d2bb22d47bf50562d5c7090cbc9e36896e5d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-08-30 15:52:26 -0300

    Saved callie/main.py @2021-08-30 18:52

--- callie/main.py
+++ callie/main.py
@@ -9,7 +9,7 @@ def Historia():
                  tit="Mary",
                  style=dict(left=90, top=60, width=230, height=200))
     mary.entra(stormCena)
-    txtmary= Texto(stormcena,
+    txtmary = Texto (stormCena,
                          "Mary is salling for a long time. At the morning, she was just on a trip, but she went too far, and now, 4:00pm, Mary is fighting agains a big storm.")
     mary.vai = txtmary.vai
     stormCena.vai()
