bc9a8f397041b27a91ceb14b8c703ae31ec2e585
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-13 13:43:46 -0300

    Saved kellee/__score__.py at Timestamp: 2022-07-13 16:43:45

--- kellee/__score__.py
+++ kellee/__score__.py
@@ -6,3 +6,5 @@
 "f2c26a1d": {"author_nick": "", "author_name": "", "author_email": "hello@Daniel", "author_org": "", "author_site": "", "author_public": "on"},[
 ],
 "40e3bcfa": {"author_nick": "liesquinelato", "author_name": "Lilian Esquinelato", "author_email": "lilianes93@gmail.com", "author_org": "", "author_site": "Professora", "author_public": "on"},[
+],
+"c4d6de39": {"author_nick": "", "author_name": "", "author_email": "helo@gmail", "author_org": "", "author_site": "", "author_public": "on"},[
